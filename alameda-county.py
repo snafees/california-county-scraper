537a5b5209e178c15a7ee1f0da453e70c6908cd5
johnlewissims <johnsimsemail@gmail.com>
2020-03-27 07:59:35 -0400

    Chromedriver Added. Stats pulled and cleaned from Alameda Site.

---
+++ alameda-county.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Imports. Make sure webdriver is executableby running
+# 'chmod +x ./chromedriver' in directory
+ 
+from selenium import webdriver
+from bs4 import BeautifulSoup
+alameda = "http://www.acphd.org/2019-ncov.aspx" 
+driver = webdriver.Chrome('./chromedriver')
+driver.get(alameda)
+html = driver.page_source
+soup = BeautifulSoup(html)
+
+# Select Figures
+positiveCasesRaw = soup.find("table", {"class": "contacts_table"}).div.select('em')[0].contents[0]
+deathsRaw = soup.find("table", {"class": "contacts_table"}).div.select('em')[1].contents[0]
+
+# Remove *
+positiveCasesClean = positiveCasesRaw.replace("*", "")
+deathsClean = positiveCasesRaw.replace("*", "")
