5f82df77a1ae6fbbda0e10e9e315b5e9d841bb7e
Saba Nafees <saba.nafees314@gmail.com>
2020-03-27 15:06:07 -0500

    added small change

--- alameda-county.py
+++ alameda-county.py
@@ -8,8 +8,8 @@ alameda = "http://www.acphd.org/2019-ncov.aspx"
 driver = webdriver.Chrome('./chromedriver')
 driver.get(alameda)
 html = driver.page_source
-soup = BeautifulSoup(html)
-
+soup = BeautifulSoup(html, features="html.parser")
+print(soup)
 # Select Figures
 positiveCasesRaw = soup.find("table", {"class": "contacts_table"}).div.select('em')[0].contents[0]
 deathsRaw = soup.find("table", {"class": "contacts_table"}).div.select('em')[1].contents[0]
@@ -17,3 +17,6 @@ deathsRaw = soup.find("table", {"class": "contacts_table"}).div.select('em')[1].
 # Remove *
 positiveCasesClean = positiveCasesRaw.replace("*", "")
 deathsClean = positiveCasesRaw.replace("*", "")
+print("Num of positive cases in Alameda County" + positiveCasesClean)
+print(deathsClean)
+
