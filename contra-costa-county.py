d4bc543a2f781040eda823f2343bdfd56e8d6009
johnlewissims <johnsimsemail@gmail.com>
2020-03-27 18:08:06 -0400

    Santa Clara Added

--- contra-costa-county.py
+++ contra-costa-county.py
@@ -8,8 +8,5 @@ page = requests.get(url)
 contents = page.content
 soup = BeautifulSoup(contents, 'html.parser')
 
-
 positiveCases = soup.body.find(id="comp-k7p0eq5v").h1.span.span.contents[0]
 deaths = soup.body.find(id="comp-k7p0g5md1").h1.span.span.contents[0]
-
-print(deaths)
--- santa-clara.py
+++ santa-clara.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
-# Imports. Make sure webdriver is executableby running 'chmod +x ./chromedriver' in directory
- 
 from selenium import webdriver
 from bs4 import BeautifulSoup
-alameda = "http://www.acphd.org/2019-ncov.aspx" 
+import time
+
+alameda = "https://app.powerbigov.us/view?r=eyJrIjoiODI1YmRlMjUtODUwOC00ZDE0LWExMjMtMjA2ZDI2MTRlMGE4IiwidCI6IjBhYzMyMDJmLWMzZTktNGY1Ni04MzBkLTAxN2QwOWQxNmIzZiJ9" 
 driver = webdriver.Chrome('./chromedriver')
 driver.get(alameda)
+
+time.sleep(10)
 html = driver.page_source
 soup = BeautifulSoup(html)
 
+print(soup.prettify())
+
 # Select Figures
-positiveCasesRaw = soup.find("table", {"class": "contacts_table"}).div.select('em')[0].contents[0]
+positiveCases = soup.find_all("text", {"class": "value"})[0].title.contents[0]
+
 deathsRaw = soup.find("table", {"class": "contacts_table"}).div.select('em')[1].contents[0]
 
 # Remove *
