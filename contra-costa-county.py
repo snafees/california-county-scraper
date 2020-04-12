223e2ead3e966e8d3078727443c4061970dcd4f5
Saba Nafees <saba.nafees314@gmail.com>
2020-04-12 00:16:49 -0500

    adding updated contra-costa-county.py

--- contra-costa-county.py
+++ contra-costa-county.py
@@ -3,10 +3,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.coronavirus.cchealth.org/" 
+url = "https://socoemergency.org/"
 page = requests.get(url)
 contents = page.content
 soup = BeautifulSoup(contents, 'html.parser')
+print(soup.prettify())
 
 
 positiveCases = soup.body.find(id="comp-k7p0eq5v").h1.span.span.contents[0]
